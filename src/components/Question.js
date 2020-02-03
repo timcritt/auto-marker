5034a699a46d0b637dfb94fce9c85aa1ca9b84e7
Timothy Crittenden <tim_critt@hotmail.com>
2020-02-03 02:32:35 +0100

    modified:   src/components/Question.js 	modified:   src/components/Quiz.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -11,7 +11,7 @@ class Question extends React.Component {
   render() {
     return (
       <div>
-        {this.props.question.question}
+        <p>{this.props.question.question}</p>
         <input onChange={this.updateUserAnswer}></input>
         <button onClick={this.checkUserAnswer}>Check</button>
         {this.state.correctlyAnswered && this.state.answered ? (
@@ -24,6 +24,7 @@ class Question extends React.Component {
         ) : (
           ''
         )}
+        
       </div>
     )
   }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Question from './Question';
-
+import sampleQuestions from "../sampleQuestions";
 
 class Quiz extends React.Component {
-  
+  state = {
+    questions: ["Question 1", "Question 2", "Question 3"],
+  }
+  loadSampleQuestions = () => {
+    const questions = sampleQuestions;
+    this.setState( {
+      questions
+    })
+  }
+
   render() {
     return (
       <div>
-        <button onClick={this.props.loadSampleQuestions}>Load Sample Questions</button>
+        <button onClick={this.loadSampleQuestions}>Load Sample Questions</button>
         <ul>
-          {Object.keys(this.props.questions)
+          {Object.keys(this.state.questions)
             .map(key => 
               <Question 
                 key={key}
